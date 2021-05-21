f78beb2137986c89370ba51d3f16769148f5bc11
Sergey Blednov <sergey.blednov@pixacore.com>
2021-05-20 20:51:54 -0400

    Lecture 1

--- Memorize/ContentView.swift
+++ Memorize/ContentView.swift
@@ -9,8 +9,13 @@ import SwiftUI
 
 struct ContentView: View {
     var body: some View {
-        Text("Hello, world!")
-            .padding()
+        ZStack {
+            RoundedRectangle(cornerRadius: 20)
+                .stroke(lineWidth: 3)
+            Text("Placeholder")
+        }
+        .padding(.horizontal)
+        .foregroundColor(/*@START_MENU_TOKEN@*/.red/*@END_MENU_TOKEN@*/)
     }
 }
 
