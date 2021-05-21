f4409f471e7b03c450d4b73f570eae8928d96dd1
Sergey Blednov <sergey.blednov@pixacore.com>
2021-05-20 20:03:37 -0400

    Initial Commit

---
+++ Memorize/MemorizeApp.swift
@@ -0,0 +1,17 @@
+//
+//  MemorizeApp.swift
+//  Memorize
+//
+//  Created by Sergiy Blednov on 5/20/21.
+//
+
+import SwiftUI
+
+@main
+struct MemorizeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
