6ec8c1aa658055c542d3694ee519c7a9f3bd8be1
Sergey Blednov <sergey.blednov@pixacore.com>
2021-05-23 17:56:41 -0400

    Lecture 2

--- Memorize/ContentView.swift
+++ Memorize/ContentView.swift
@@ -4,23 +4,90 @@
 //
 //  Created by Sergiy Blednov on 5/20/21.
 //
-
+ 
 import SwiftUI
 
 struct ContentView: View {
+    var emojis = ["🚗", "🚕", "🚌", "🚎", "🏎", "🚓", "🚑", "🚒", "🚐", "🛻", "🚛", "🛺", "🚁", "🚀", "🛥", "🚤", "⛵️", "⛴", "🚲", "🏍", "🚂", "🚚", "✈️", "🚜"]
+    @State var emojiCount = 6
+        
     var body: some View {
-        ZStack {
-            RoundedRectangle(cornerRadius: 20)
-                .stroke(lineWidth: 3)
-            Text("Placeholder")
+        VStack {
+            ScrollView {
+                LazyVGrid(columns: [GridItem(.adaptive(minimum: 65))]) {
+                    ForEach(emojis[0..<emojiCount], id: \.self) { emoji in
+                        CardView(content: emoji)
+                            .aspectRatio(2/3, contentMode: .fit)
+                    }
+                }
+            }
+            .foregroundColor(.red)
+            Spacer()
+            HStack {
+                remove
+                Spacer()
+                add
+            }
+            .font(.largeTitle)
+            .padding(.horizontal)
         }
         .padding(.horizontal)
-        .foregroundColor(/*@START_MENU_TOKEN@*/.red/*@END_MENU_TOKEN@*/)
+    }
+    
+    var remove: some View {
+        VStack {
+            Button {
+                if emojiCount > 1 {
+                    emojiCount -= 1
+                }
+            } label: {
+                VStack {
+                    Image(systemName: "minus.circle")
+                }
+            }
+        }
+    }
+    var add: some View {
+        VStack {
+            Button {
+                if emojiCount < emojis.count {
+                    emojiCount += 1
+                }
+
+            } label: {
+                VStack {
+                    Image(systemName: "plus.circle")
+                }
+            }
+        }
+    }
+}
+
+struct CardView: View {
+    var content: String
+    @State var isFaceUp: Bool = true
+    var body: some View {
+        ZStack {
+            let shape = RoundedRectangle(cornerRadius: 20)
+            if isFaceUp {
+                shape.fill().foregroundColor(.white)
+                shape.strokeBorder(lineWidth: 3)
+                Text(content).font(.largeTitle)
+            } else {
+                shape.fill()
+            }
+        }
+        .onTapGesture {
+            isFaceUp = !isFaceUp
+        }
     }
 }
 
 struct ContentView_Previews: PreviewProvider {
     static var previews: some View {
         ContentView()
+            .preferredColorScheme(.dark)
+        ContentView()
+            .preferredColorScheme(.light)
     }
 }
